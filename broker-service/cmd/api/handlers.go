2eef3d20b2d36887bc40b1d8d3c94891b17ade30
shubhamkumar96 <shubhamkumarranu@gmail.com>
2024-07-26 16:18:26 +0530

    Added button to call logger-service via broker-service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -11,12 +11,19 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 // payload for auth-service
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+// payload for logger-service
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +44,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logData(w, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
@@ -52,6 +61,7 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		app.errorJSON(w, err)
 		return
 	}
+	request.Header.Set("Content-Type", "application/json")
 
 	// call the auth-service
 	client := &http.Client{}
@@ -88,6 +98,41 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	payLoad.Error = false
 	payLoad.Message = "Authenticated!"
 	payLoad.Data = jsonFromService.Data
+
+	// write to response
+	app.writeJSON(w, http.StatusAccepted, payLoad)
+}
+
+func (app *Config) logData(w http.ResponseWriter, l LogPayload) {
+	// create json that we will send to the logger-service
+	jsonData, _ := json.MarshalIndent(l, "", "\t")
+
+	// create a http request
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	// call the logger-service
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// get the correct status-code
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth-service"))
+		return
+	}
+
+	var payLoad jsonResponse
+	payLoad.Error = false
+	payLoad.Message = "Logged!"
 
 	// write to response
 	app.writeJSON(w, http.StatusAccepted, payLoad)
