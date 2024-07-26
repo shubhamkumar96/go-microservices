381e1ce2f7c5282a2db93fc17d60ee3da50edac0
shubhamkumar96 <shubhamkumarranu@gmail.com>
2024-07-26 16:55:55 +0530

    Calling logger-service from auth-service

--- auth-service/cmd/api/handlers.go
+++ auth-service/cmd/api/handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -31,6 +33,13 @@ func (app *Config) Authenticate(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// log successful authentication to logger-service
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil {
+		app.errorJSON(res, err)
+		return
+	}
+
 	payLoad := jsonResponse{
 		Error:   false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
@@ -38,4 +47,33 @@ func (app *Config) Authenticate(res http.ResponseWriter, req *http.Request) {
 	}
 
 	app.writeJSON(res, http.StatusAccepted, payLoad)
+}
+
+func (app *Config) logRequest(name, data string) error {
+	var logEntry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	logEntry.Name = name
+	logEntry.Data = data
+
+	// create json that we will send to the logger-service
+	jsonData, _ := json.MarshalIndent(logEntry, "", "\t") // In production, use 'Marshal()' not 'MarshalIndent()'.
+
+	// create a http request
+	request, err := http.NewRequest("POST", "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	// call the logger-service
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
